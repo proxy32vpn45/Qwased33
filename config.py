3237af91f4e738e75cb040b0f10529fcf375d42f
proxy32vpn45 <halva255550@gmail.com>
2026-06-08 11:58:28 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# =========================
+# CONFIG
+# =========================
+
+SOURCE_SUBS = [
+    # добавляешь свои ссылки сюда
+]
+
+REMOVE_DUPLICATES = True
+OUTPUT_FILE = "output.txt"
+
+SUPPORTED = [
+    "vless://",
+    "vmess://",
+    "trojan://",
+    "ss://",
+]
